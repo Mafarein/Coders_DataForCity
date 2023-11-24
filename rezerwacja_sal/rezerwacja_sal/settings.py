32e900affff0c9e939a2a8c8cb69133d632d6e8b
Barbara Jurzysta <01171236@pw.edu.pl>
2023-11-24 18:50:34 +0100

    Css dla strony logowania

--- rezerwacja_sal/rezerwacja_sal/settings.py
+++ rezerwacja_sal/rezerwacja_sal/settings.py
@@ -125,6 +125,11 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "app-messages")
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+ os.path.join(BASE_DIR,'main', 'static'),
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
