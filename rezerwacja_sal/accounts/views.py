9e3ba264276af1b3f9c2a11fb2a4c7bd61f2d0ff
wiktoria <wiktoriakoniecko@gmail.com>
2023-11-26 17:03:53 +0100

    dodana możliwość akceptacji rezerwacji

--- rezerwacja_sal/accounts/views.py
+++ rezerwacja_sal/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
@@ -7,6 +7,7 @@ from django.core.mail import EmailMessage
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .forms import *
 from .tokens import account_activation_token
 
@@ -64,4 +65,7 @@ def registration_choice(request):
 
 @login_required
 def profile(request):
+    usr = get_user(request)
+    if usr.groups.filter(Q(name="SportFacilityOwners") | Q(name="Schools")).exists():
+        return redirect("user_facilities", uid=usr.id)
     return render(request, "accounts/profile.html", {"user": get_user(request)})
--- rezerwacja_sal/main/urls.py
+++ rezerwacja_sal/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('usr/<int:uid>/', get_facilities, name="user_facilities"),
     path('obj/create/', create_facility, name="facility_create"),
     path('obj/<int:fid>/', facility_detail, name="facility_detail"),
+    path('obj/<int:fid>/res/<int:rid>', accept_reservation, name="accept_reservation")
 ]
